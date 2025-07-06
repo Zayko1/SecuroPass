6edf9f02178f5f119b9460b6f9b7ec4f7bca3cf9
Zayko1 <rrouibi@myges.fr>
2025-07-06 15:11:26 +0200

    Remove creds

--- desktop/config.py
+++ desktop/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST", "217.154.24.244"),
-    "user": os.getenv("DB_USER", "root"),
-    "port": int(os.getenv("DB_PORT", "3306")),
-    "password": os.getenv("DB_PASSWORD", "IndienTéméraire19%"),
-    "database": os.getenv("DB_NAME", "securopass")
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "port": int(os.getenv("DB_PORT")),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME")
 }
